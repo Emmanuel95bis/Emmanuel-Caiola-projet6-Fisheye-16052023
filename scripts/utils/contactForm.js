f3f1ea0c240487e5972e8b739a768f90ef98a812
Emmanuel Caiola <emmanuel.caiola@free.fr>
2023-06-13 20:12:54 +0200

    effacement formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -13,5 +13,19 @@ if (e.key == "Escape") closeModal();
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    document.formContactMe.reset();
     modal.style.display = "none";
 }
+
+
+const inputLN = document.getElementById("inputLastName");
+const inputFN = document.getElementById("inputFirstName");
+const inputEmail = document.getElementById("inputEmail");
+
+const submitButton = document.querySelector(".contact_button");
+
+submitButton.addEventListener("click", function() {
+  console.log(inputLN.value);
+  console.log(inputFN.value);
+  console.log(inputEmail.value);
+});
